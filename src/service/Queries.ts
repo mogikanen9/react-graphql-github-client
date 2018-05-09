b340096e9d3310e4885314d640cfbf724c2d2288
Vlad Voinov <mogikanensoftware@gmail.com>
2018-05-09 10:49:42 -0400

    buildOrganizationRepoQuery

--- src/service/Queries.ts
+++ src/service/Queries.ts
@@ -1,5 +1,5 @@
-const LIST_ORG_REPOS: string = 
-`query OrganizationForLearningReact {
+const LIST_ORG_REPOS: string =
+  `query OrganizationForLearningReact {
     organization(login: "spring-projects") {
       name
       url
@@ -19,5 +19,26 @@ const LIST_ORG_REPOS: string =
     }
   }`;
 
+export function buildOrganizationRepoQuery(itemsPerPage: number, cursorId: string): string {
+  return `query OrganizationRepos {
+    organization(login: "spring-projects") {
+      name
+      url
+      repositories(first: ${itemsPerPage}, after "${cursorId}") {
+        edges {
+          node {
+            name,
+            description,
+            createdAt
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }`
+};
 
 export { LIST_ORG_REPOS };
