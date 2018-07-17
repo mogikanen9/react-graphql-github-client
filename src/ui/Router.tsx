0fd76f9e599b5e4b87cc7ff0235c616980258ab5
Vlad Voinov <mogikanensoftware@gmail.com>
2018-07-16 22:40:10 -0400

    done

--- src/ui/Router.tsx
+++ src/ui/Router.tsx
@@ -9,7 +9,7 @@ import { IRouterState } from './IRouterState';
 import { RepoPagination } from './page/repo/RepoPagination';
 
 const DEFAULT_PAGE_SIZE = 10;
-const DEFAULT_ORG = "spring-projects";
+const DEFAULT_ORG_NAME = "spring-projects";
 
 class Router extends React.Component<IRouterProps, IRouterState>{
 
@@ -22,43 +22,19 @@ class Router extends React.Component<IRouterProps, IRouterState>{
             isError: false,
             isLoading: false,
             repoPagination: EMPTY_REPO_PAGINATION,
-            repositories: []
+            repositories: [],
+            selectedOrgName: DEFAULT_ORG_NAME
         }
 
         this.nextRepoPage = this.nextRepoPage.bind(this);
         this.prevRepoPage = this.prevRepoPage.bind(this);
+        this.loadRepo = this.loadRepo.bind(this);
         this.repoPage = this.repoPage.bind(this);
         this.orgNameChanged = this.orgNameChanged.bind(this);
     }
 
     public componentDidMount() {
-
-        this.setState({ isLoading: true });
-        this.props.clientService.listRepos(DEFAULT_ORG, DEFAULT_PAGE_SIZE)
-            .then((result: RepositoryResultList) => {
-
-                const pageCursors = this.state.repoPagination.pageCursors;
-                if (!pageCursors.has(result.paginationInfo.currentPageCursor)) {
-                    pageCursors.set(result.paginationInfo.currentPageCursor, EMPTY_PAGE_CURSOR);
-                }
-                this.setState(
-                    {
-                        errorMessage: '',
-                        isError: false,
-                        isLoading: false,
-                        repoPagination: new RepoPagination(result.paginationInfo, pageCursors),
-                        repositories: result.repos,
-                    });
-            }).catch((err) => {
-                this.setState(
-                    {
-                        errorMessage: err,
-                        isError: true,
-                        isLoading: false,
-                        repoPagination: EMPTY_REPO_PAGINATION
-                    }
-                );
-            });
+        this.loadRepo(this.state.selectedOrgName);
     }
 
 
@@ -67,7 +43,7 @@ class Router extends React.Component<IRouterProps, IRouterState>{
         const props: IViewListProps = {
             onNext: this.nextRepoPage,
             onOrgNameChange: this.orgNameChanged,
-            onPrev: this.prevRepoPage,           
+            onPrev: this.prevRepoPage,
             orgName: 'spring-projects',
             repoPagination: this.state.repoPagination,
             repos: this.state.repositories
@@ -95,9 +71,43 @@ class Router extends React.Component<IRouterProps, IRouterState>{
         };
     }
 
+    protected loadRepo(orgName: string): void {
+        this.setState({ isLoading: true });
+        this.props.clientService.listRepos(orgName, DEFAULT_PAGE_SIZE)
+            .then((result: RepositoryResultList) => {
+
+                const pageCursors = this.state.repoPagination.pageCursors;
+                if (!pageCursors.has(result.paginationInfo.currentPageCursor)) {
+                    pageCursors.set(result.paginationInfo.currentPageCursor, EMPTY_PAGE_CURSOR);
+                }
+                this.setState(
+                    {
+                        errorMessage: '',
+                        isError: false,
+                        isLoading: false,
+                        repoPagination: new RepoPagination(result.paginationInfo, pageCursors),
+                        repositories: result.repos,
+                    });
+            }).catch((err) => {
+                this.setState(
+                    {
+                        errorMessage: err,
+                        isError: true,
+                        isLoading: false,
+                        repoPagination: EMPTY_REPO_PAGINATION
+                    }
+                );
+            });
+    }
+
     protected orgNameChanged(newOrgName: string): void {
-        // tslint:disable-next-line:no-console
-        console.log(`New orgName->${newOrgName}`);
+        this.setState(
+                    {
+                        repoPagination: EMPTY_REPO_PAGINATION,
+                        repositories: [],
+                        selectedOrgName: newOrgName,
+                    });
+        this.loadRepo(newOrgName);
     }
 
     protected nextRepoPage(): void {
@@ -110,7 +120,7 @@ class Router extends React.Component<IRouterProps, IRouterState>{
 
     protected repoPage(pageSize: number, paginationCursor: string, forward: boolean): void {
         this.setState({ isLoading: true });
-        this.props.clientService.listRepos(DEFAULT_ORG, pageSize, paginationCursor)
+        this.props.clientService.listRepos(this.state.selectedOrgName, pageSize, paginationCursor)
             .then((result: RepositoryResultList) => {
                 const pageCursors = this.state.repoPagination.pageCursors;
                 if (forward && !pageCursors.has(result.paginationInfo.currentPageCursor)) {
